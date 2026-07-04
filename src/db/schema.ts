dfa910ba677ba5aab4ea6479060cf7f638321ed1
Maxim Velesyuk <max.velesyuk@gmail.com>
2026-07-04 21:12:25 +0200

    update tokens schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,8 +3,11 @@ import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 export const tokensTable = sqliteTable('tokens', {
   id: int().primaryKey({ autoIncrement: true }),
   server: text().notNull(),
-  token: text().notNull(),
+  access_token: text().notNull(),
   refresh_token: text().notNull(),
-  created_at: int().notNull(),
   expires_in: int().notNull(),
+  created_at: int().notNull(),
+  created_at_str: text().notNull(),
+  expires_at: int().notNull(),
+  expires_at_str: text().notNull(),
 })
